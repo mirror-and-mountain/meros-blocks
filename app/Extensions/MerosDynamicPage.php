21af187de3464ae829e1b6fe244e50650d13a461
tmw-mm <toby@mirrorandmountain.com>
2025-05-07 14:47:09 +0100

    Commits for pre-release 0.0.5

--- app/Extensions/MerosDynamicPage.php
+++ app/Extensions/MerosDynamicPage.php
@@ -9,5 +9,8 @@ class MerosDynamicPage extends Feature
     protected function override(): void
     {
         $this->enabled = true;
+        add_filter( 'meros_' . $this->name . '_settings_section_title', function ( $_title ) {
+            return 'Dynamic Page';
+        });
     }
 }
--- app/Theme.php
+++ app/Theme.php
@@ -9,7 +9,6 @@ class Theme extends ThemeManager
     protected function configure(): void
     {
         $this->enable_smooth_scrolling = true;
-        $this->use_single_page_loading = true;
         $this->use_unified_settings_pages = true;
     }
 }
