d71abd468f959ae1fc6c47deeeed67c188d2750a
Toby Wilson <109956652+tmw-mm@users.noreply.github.com>
2025-06-09 14:20:49 +0000

    Bump meros-dynamic-page version

---
+++ patterns/meros-blocks-header.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Meros Blocks Header
+ * Slug: meros-blocks/meros-blocks-header
+ * Categories: Headers
+ */
+?>
+<!-- wp:meros/dynamic-header {"isOverlay":true,"textStartColor":"#ffffff"} -->
+<header class="wp-block-meros-dynamic-header" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)" data-sticky="true" data-bottom-offset="true" data-overlay="true" data-background-start="#FFFFFF00" data-background-end="#FFFFFF" data-text-start="#ffffff" data-text-end="#000000"><!-- wp:group {"layout":{"type":"flex","orientation":"horizontal","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","orientation":"horizontal"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":60} /-->
+
+<!-- wp:site-title /--></div>
+<!-- /wp:group -->
+
+<!-- wp:navigation {"ref":8,"openSubmenusOnClick":true,"icon":"menu","enableMerosNav":true,"merosNavDirection":"top","merosNavBgColor":"#ffffff","merosNavDesktopHighlight":true} /--></div>
+<!-- /wp:group --></header>
+<!-- /wp:meros/dynamic-header -->
