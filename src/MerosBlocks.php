d4303d6ce22f25731d0c285686e20a989c900596
tmw-mm <toby@mirrorandmountain.com>
2026-05-07 16:46:30 +0100

    Update for new framework compatibility

--- src/MerosBlocks.php
+++ src/MerosBlocks.php
@@ -5,33 +5,29 @@ namespace MM\Meros\Blocks;
 use MM\Meros\App\Package;
 
 class MerosBlocks extends Package {
+    public string $name             = "Meros Blocks";
     public string $author           = "Meros";
+    public string $handle           = "meros_blocks";
     public string $authorUri        = "https://merosblocks.com";
     public string $authorSupportUri = "https://merosblocks.com/support";
     public string $description      = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua';
 
-    protected bool $discoverBlocks = true;
-    protected bool $discoverAssets = true;
-
     protected function configure(): void {
-        add_filter('meros_form_field_block_is_switchable', '__return_false');
-        add_filter('meros_form_row_block_is_switchable', '__return_false');
-        add_filter('meros_form_section_block_is_switchable', '__return_false');
-        add_filter('meros_mega_menu_column_block_is_switchable', '__return_false');
-        add_filter('meros_swiper_slide_block_is_switchable', '__return_false');
+        $this->blocks()->discover();
+        $this->assets()->discover();
         
-        // // Nav filters
+        // Nav filters
         add_filter('render_block', [Filters::class, 'renderAdvancedNav'], 10, 2);
         add_filter('render_block', [Filters::class, 'renderAdvancedNavSubmenu'], 10, 2);
         add_filter('render_block', [Filters::class, 'renderAdvancedNavLink'], 10, 2);
 
-        // // Block args filters
+        // Block args filters
         add_filter('register_block_type_args', [Filters::class, 'registerAdditionalArgs'], 10, 2);
 
-        // // Block FX filters
+        // Block FX filters
         add_filter('render_block', [Filters::class, 'renderBlockFxBlocks'], 10, 2);
 
-        // // Form Filters
+        // Form Filters
         add_filter('allowed_block_types_all', [Filters::class, 'restrictFormBlocks'], 10, 2);
     }
 }
